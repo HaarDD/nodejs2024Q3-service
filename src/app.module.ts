b36fd2cdf8798a12f9b9b97b55ec2883c9ca5191
HaarDD <maxgalitskiy@gmail.com>
2024-11-10 21:53:18 +0300

    feat: remove AppController and AppService, restructure AppModule with new feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './modules/user.module';
+import { ArtistModule } from './modules/artist.module';
+import { AlbumModule } from './modules/album.module';
+import { TrackModule } from './modules/track.module';
+import { RepositoryModule } from './modules/repository.module';
+import { FavoritesModule } from './modules/favorites.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoritesModule,
+    RepositoryModule,
+  ],
 })
 export class AppModule {}
