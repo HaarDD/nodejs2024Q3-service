bf371d0def53230601a77ef5ddf5e347a6befdaa
HaarDD <maxgalitskiy@gmail.com>
2024-11-11 01:14:08 +0300

    feat: integrate Swagger documentation with dynamic API specification loading

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { loadApiSpec } from './utils/swagger.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,28 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const apiSpec = loadApiSpec();
+
+  const config = new DocumentBuilder()
+    .setTitle(apiSpec.info.title)
+    .setDescription(apiSpec.info.description)
+    .setVersion(apiSpec.info.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  Object.assign(document, {
+    components: apiSpec.components,
+    paths: apiSpec.paths,
+  });
+
+  SwaggerModule.setup('doc', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
 
   await app.listen(4000);
 }
