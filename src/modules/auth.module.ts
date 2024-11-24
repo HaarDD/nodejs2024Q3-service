8391f984d828fadc5005c49350e8707f81157486
HaarDD <maxgalitskiy@gmail.com>
2024-11-24 20:36:16 +0300

    feat: implement AuthModule with JWT authentication and guards

---
+++ src/modules/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { RepositoryModule } from '../modules/repository.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get('TOKEN_EXPIRE_TIME'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    RepositoryModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [JwtAuthGuard],
+})
+export class AuthModule {}
